b361d1c90c7238b77b606487c484eef9e473bc39
Sarah Drew <drew.sarah.e@gmail.com>
2021-07-28 18:53:26 -0700

    changed topbar from materialui to headroom

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,41 +1,15 @@
 import React from "react";
-import Slide from '@material-ui/core/Slide';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Headroom from 'react-headroom'
 import "./top-bar.scss"
 
-//is going to be used to make panda appear
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("nav-container").style.fontSize = "30px";
-//   } else {
-//     document.getElementById("nav-container").style.fontSize = "90px";
-//   }
-// }
-
-
-function HideOnScroll(props) {
-    const { children } = props;
-    const trigger = useScrollTrigger();
-  
-    return (
-      <Slide appear={false} direction="down" in={!trigger}>
-        {children}
-      </Slide>
-    );
-  }
-  
-  export default function TopBar(props) {
-    return (
-      <React.Fragment>
-        <HideOnScroll {...props}>
-            <div id="nav-container">
-                <a href="#reviews">Reviews</a>
-                <a href="#upgrades">Upgrades</a>
-                <a href="#contact">Contact</a>
-            </div>
-        </HideOnScroll>
-      </React.Fragment>
-    );
-  }
+export default function TopBar(props) {
+  return (
+    <Headroom>
+        <div id="nav-container">
+            <a href="#reviews">Reviews</a>
+            <a href="#upgrades">Upgrades</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </Headroom>
+  );
+}

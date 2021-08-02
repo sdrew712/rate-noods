fbcb8fc9d898e13804741d334b5663b3c970f8d8
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-01 18:20:31 -0700

    used react router to link to multiple pages

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 import Headroom from 'react-headroom'
 import "./top-bar.scss"
+import Reviews from "../Reviews/Reviews"
+import Upgrades from "../Upgrades/Upgrades"
+import Contact from "../Contact/Contact"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 export default function TopBar(props) {
   return (
     <Headroom>
+      <Router>
         <div id="nav-container">
-            <a href="#reviews">Reviews</a>
-            <a href="#upgrades">Upgrades</a>
-            <a href="#contact">Contact</a>
+          <nav>
+                <Link to="/reviews">Reviews</Link>
+                <Link to="/upgrades">Upgrades</Link>
+                <Link to="/contact">Contact</Link>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/reviews">
+              <Reviews />
+            </Route>
+            <Route path="/upgrades">
+              <Upgrades />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
         </div>
+      </Router>
     </Headroom>
   );
 }

af167a3f4d371c92ba11696d0a2e76f76cb302b9
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-05 17:53:47 -0700

    fixed links in HomeBody

--- src/components/Home/HomeBody/HomeBody.jsx
+++ src/components/Home/HomeBody/HomeBody.jsx
@@ -15,34 +15,14 @@ export default function HomeBody(){
       <div className="style-1">
         <h2 className="header-text">Hello!</h2>
         <p className="style-1-p">I’m an instant noodle addict aspiring to try as many noodles as possible.</p>
-        <Router>
-          <div>
-            <nav>
-              <Link to="/reviews" className="button">See my noodle reviews</Link>
-            </nav>
-            <Switch>
-              <Route path="/reviews">
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+        <Link to="/reviews" className="button">See my noodle reviews</Link>
       </div>
 
       <div className="style-2">
         <div className="container-2">
-        <h2 className="header-text2">Goodbye plain noodles!</h2>
-        <p>Want to learn how to make your noodles more of a meal?</p>
-        <Router>
-          <div>
-            <nav>
-              <Link to="/upgrades" className="button">Upgrade your noodles</Link>
-            </nav>
-            <Switch>
-              <Route path="/upgrades">
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+          <h2 className="header-text2">Goodbye plain noodles!</h2>
+          <p>Want to learn how to make your noodles more of a meal?</p>
+          <Link to="/upgrades" className="button">Upgrade your noodles</Link>
         </div>
         <img src={Meal} alt="tasty_meal" className="meal"/>
       </div>
@@ -50,17 +30,7 @@ export default function HomeBody(){
       <div className="style-1">
         <h2 className="header-text">Get in touch</h2>
         <p className="style-1-p">Have questions?  Comments?  Like my work?</p>
-        <Router>
-          <div>
-            <nav>
-              <Link to="/contact" className="button">Contact me</Link>
-            </nav>
-            <Switch>
-              <Route path="/contact">
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+        <Link to="/contact" className="button">Contact me</Link>
       </div>
     </>       
   )

54332732099f4bcd5376e15a44afd847e53f52a6
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-07 12:49:51 -0700

    fixed burger padding problem

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -9,15 +9,17 @@ import { Link } from "react-router-dom";
 export default function TopBar() {
   return (
     <Headroom className="headroom">
-        <Link to="/"><img src={Panda} className="panda-nav" alt="panda_img"/></Link>
+      <Link to="/"><img src={Panda} className="panda-nav" alt="panda_img"/></Link>
 
+      <div id="topbar-padding">
         <Burger />
-        
-        <div id="top-nav">
-          <Link to="/reviews" className="nav-link">Reviews</Link>
-          <Link to="/upgrades" className="nav-link">Upgrades</Link>
-          <Link to="/contact" className="nav-link">Contact</Link>
-        </div>
+      </div>
+
+      <div id="top-nav">
+        <Link to="/reviews" className="nav-link">Reviews</Link>
+        <Link to="/upgrades" className="nav-link">Upgrades</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+      </div>
     </Headroom>
   );
 }

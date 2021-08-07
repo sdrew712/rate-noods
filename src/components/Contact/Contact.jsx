cedafa2876077c4bbe393820273903a09b06c082
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-07 12:53:27 -0700

    started contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Contact(){
-  
+  return(
+    <>
+      <h1>sup world</h1>
+    </>
+  );
 }
--- src/components/Home/HomeBody/HomeBody.jsx
+++ src/components/Home/HomeBody/HomeBody.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import "./home-body.scss";
 import Meal from "./meal.jpg";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export default function HomeBody(){
